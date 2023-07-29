3cd22422d93f6bdd70e9c7e3fc8a26314a316643
ZubairAhmmed <mdzubairahmedshaik22@gmail.com>
2023-07-30 04:12:16 +0530

    String Basics Assignment committed
    String Basics Assignment committed 22/07/2023

---
+++ 04_stringBasicAssign1.js
@@ -0,0 +1,18 @@
+
+
+function stingBasics(){
+    console.log(`My dream company name is=>${comp}`);
+    var yourhoby1 = "learning";
+    var yourhoby2 = "reading";
+    var yourhoby3 = "writing";
+    console.log(`My hobies are:`,yourhoby1,yourhoby2,yourhoby3);
+    console.log(`my hobies are : ${yourhoby1+yourhoby2+yourhoby3}`);
+    
+}
+var comp = "GoogleTach";
+
+stingBasics();
+var yourhoby1 = "learning";
+    var yourhoby2 = "reading";
+    var yourhoby3 = "writing";
+    console.log(`my hobies are : ${yourhoby1+yourhoby2+yourhoby3}`);
